bb153a74fbcbf0f894668d025f5279db5f4ae506
guildsmac <guildsmac@gmail.com>
2018-08-21 09:14:06 -0300

    Recovering User data

--- Controle/readJson/login.php
+++ Controle/readJson/login.php
@@ -1,11 +1,12 @@
 <?php
 //SENHA 1: MgS_1998
 //SENHA 2: Eoqviado
-    include '../config.php';
+    include 'config.php';
     $json = file_get_contents('php://input');
     $obj = json_decode($json, true);
     $cpf = $obj['cpfPessoa'];
-    $passwd = hash('sha256', $obj['senha']);
+    $passwd = $obj['senha'];
+    $passwd = hash('sha256', $passwd);
     if($obj['cpfPessoa'] != "") {
         $res = DBRead("hel_pessoa as p, hel_funcionario as f", "WHERE p.cpfPessoa = '{$cpf}' and f.senha = '{$passwd}' and p.idPessoa = f.idFuncionario", "p.*, f.salarioFuncionario");
         if($res){
