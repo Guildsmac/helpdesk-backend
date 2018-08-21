aada670e183f19c8607928ac026ac3708d9b2ee6
guildsmac <guildsmac@gmail.com>
2018-08-21 10:01:11 -0300

    Returning User informations

--- Controle/readJson/login.php
+++ Controle/readJson/login.php
@@ -1,7 +1,5 @@
 <?php
-//SENHA 1: MgS_1998
-//SENHA 2: Eoqviado
-    include 'config.php';
+    include 'C:/Users/guild/PhpstormProjects/Helpdesk Back-End/Controle/config.php';
     $json = file_get_contents('php://input');
     $obj = json_decode($json, true);
     $cpf = $obj['cpfPessoa'];
@@ -10,7 +8,7 @@
     if($obj['cpfPessoa'] != "") {
         $res = DBRead("hel_pessoa as p, hel_funcionario as f", "WHERE p.cpfPessoa = '{$cpf}' and f.senha = '{$passwd}' and p.idPessoa = f.idFuncionario", "p.*, f.salarioFuncionario");
         if($res){
-            echo json_encode('ok');
+            echo json_encode($res[0]);
         }else
             echo json_encode('error');
     }else{
